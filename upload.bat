a79f2f45aba0aa9a5c2eff2ebedb3ae7467171b9
spspider <spspider95@gmail.com>
2021-09-19 15:59:49 +0300

    first commit

--- upload.bat
+++ upload.bat
@@ -1,7 +1,3 @@
-echo "# AndroidQUIZtTester" >> README.md
-git init
 git add .
 git commit -m "first commit"
-git branch -M master
-git remote add origin https://github.com/spspider/AndroidQUIZtTester.git
-git push -u origin master
+git push
