30466e66687d53f7569fc20c6a3cb669c95e7c9e
spspider <spspider95@gmail.com>
2021-09-23 20:32:10 +0300

    first commit

---
+++ run/terraform/terraform_bash_automatic_script.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+terraform init
+terraform apply -auto-approve
+
+#ip="$(jq '[.resources[].instances[].attributes.public_dns] | sort[]' terraform.tfstate)"
+ip="$(jq '[.resources[].instances[].attributes.public_dns]  | .[]'   terraform.tfstate)"
+name="$(jq '[.resources[].instances[].attributes.tags.Name]  | .[]'   terraform.tfstate)"
+
+IFS=$'\n' read -rd '' -a ip_arr <<<"$ip"
+IFS=$'\n' read -rd '' -a name_arr <<<"$name"
+
+
+folder_ansible='../ansible' filename="ansible/hosts.ini"; rm -rf $filename; mkdir $folder_ansible
+echo '[server]' >> $filename
+for ((i = 0; i < (${#ip_arr[@]} ); i++)); do
+#for a in "${name_arr[@]}"; do 
+# name_servers=$(echo "${array[$i]}" | cut -d ""\" -f 2)
+ip_that=$(echo "${ip_arr[$i]}" | cut -d ""\" -f 2)
+name_that=$(echo "${name_arr[$i]}" | cut -d ""\" -f 2 | sed 's/ //g')
+echo  "$name_that" 'ansible_host='"$ip_that" >> $filename
+done
+
+echo '' >> $filename
+echo '[same_cred:children]' >> $filename
+echo 'server' >> $filename
+
+#directory_group_vars='group_vars'
+#mkdir "$folder_ansible/$directory_group_vars"
+
+cat $filename
+
+
+#-----------------------------now ansible turn
+
+
+ansible-inventory -i $filename --list
+#ip='[ "", "ec2-18-169-240-132.eu-west-2.compute.amazonaws.com" ]'
+
+#
+export ANSIBLE_HOST_KEY_CHECKING=False
+
+
+ansible-playbook $folder_ansible/install_docker.yml -i $folder_ansible/hosts.ini --ssh-common-args='-o StrictHostKeyChecking=no' -v
+
+#echo ip:$ip
+
+#generate ini file for ansible
+
+
---
+++ run/terraform/terraformcreatemachines.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  count = 1
+  ami           = "ami-0194c3e07668a7e36" # ubuntu 64
+  instance_type = "t2.micro"
+  key_name      = "keypair_jenkins"
+  tags = {
+    Name = "docker_terraform${count.index}"
+  }
+  security_groups = ["launch-wizard-11", ]
+  user_data       = <<EOF
+  #! /bin/bash
+  sudo apt-get update
+  sudo apt update -y
+  sudo apt install mc
+  sudo apt-get -qq install python -y
+  EOF
+  
+ #jq '[.resources[].instances[].attributes.public_dns] | sort[]' terraform.tfstate | haform.tfstate | head
+  # provisioner "remote-exec" {
+  #   inline = ["sudo apt-get -qq install python -y"]
+  # }
+
+  # connection {
+  # 	type        = "ssh"
+  # 	user        = "ubuntu"
+  # 	password    = "${rsadecrypt(aws_instance.app_server.password_data, file("keypair_jenkins.pem"))}"
+  # 	host        = "${aws_instance.app_server.public_ip}"
+  # }
+
+}
+
+
+# resource "local_file" "ec2_iini" {
+#   filename = "ec2.ini"
+#   content = <<-EOT
+#     %{ for ip in aws_instance.app_server.*.public_ip ~}
+#     ${ip} ansible_ssh_user=ec2-user
+#     %{ endfor ~}
+#   EOT
+# }
+
+output "server_public_ip" {
+  value = aws_instance.app_server[*].public_ip
+  #value >> file
+
+}
+
