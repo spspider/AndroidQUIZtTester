fac70e3728ec577508aa8cee5fb7585bea636507
spspider <spspider95@gmail.com>
2021-09-25 14:56:36 +0300

    first commit

--- run/publish_to_git.sh
+++ run/publish_to_git.sh
@@ -10,11 +10,11 @@ for ((i = 0; i < (${#name_arr[@]}); i++)); do
     name_that=$(echo "${name_arr[$i]}" | cut -d ""\" -f 2 | sed 's/ //g')
     #----------------perfom secure copy
     echo "# tTesterUKDD from machine:$name_that" >> README.md
-    path="output/$name_that/apk/debug/"
+    path="output/$name_that/apk"
     date_time=$(date +"%Y-%m-%d %T")
     git -C "$path" init 
     git -C "$path" add 
-    git -C "$path" commit -m "commit $date_time" 
+    git -C "$path" commit -m "commit $name_that at $date_time" 
     git -C "$path" branch -M master  
     git -C "$path" remote add origin git@github.com:spspider/tTesterUKDD.git  
     git -C "$path" push -u origin master 
